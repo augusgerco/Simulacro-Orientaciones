14dc3149104b21ad447f68e7d068e2a0e11034f9
augusgerco <augustosgercovich@gmail.com>
2025-04-15 08:59:29 -0300

    Simulacro COMPLETADO

---
+++ Assets/Scripts/DeQueOrientacionSos.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeQueOrientacionSos : MonoBehaviour
+{
+    public string nombreAlumno;
+    public int añoAlumno;
+    public string orientacionAlumno;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //nombre
+        if (nombreAlumno == "")
+        {
+            Debug.Log("Error");
+        }
+        //año
+        if (añoAlumno > 5 || añoAlumno < 1)
+        {
+            Debug.Log("Error");
+            return;
+        }
+
+        if (añoAlumno < 3)
+        {
+            Debug.Log("Error. Aún estás en el ciclo básico");
+            return;
+        }
+      
+        //orientacion
+        if (orientacionAlumno != "T" && orientacionAlumno != "D" && orientacionAlumno != "G" && orientacionAlumno != "M" && orientacionAlumno != "H")
+        {
+            Debug.Log("Solo puede ingresar T, D, G, M o H");
+            return;
+        }
+
+        Debug.Log("Muchas gracias " + nombreAlumno + "!");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
